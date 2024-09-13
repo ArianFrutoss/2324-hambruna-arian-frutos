ab9125986ccf9a0e8c6c189b3c02d2d86263b493
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-09-13 20:21:06 +0200

    [ADD] - Show the donut with more iron

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ fetch(`https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18cb29242
 .then((response) => response.json())
 .then((data) => {
    
-   // 1.- Mostrar donut con mas azucar 
-   
+   // 1.- Mostrar donut con mas azucar
    const donutWithMoreSugar = data.items.item.reduce((previous, current) => {
-
+      
       return current.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars > previous.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars ? current : previous;
-   });
+   })
 
    console.log("El donut con mas azucar es " + donutWithMoreSugar.name);
+   
+   // 2.- Mostrar donut con mas hierro
+   const donutWithMoreIron = data.items.item.reduce((previous, current) => {
+      
+      for (let i = 0; i < current.nutrition_facts.nutrition.vitamines.length; i++){
 
+         if (current.nutrition_facts.nutrition.vitamines[i].type === "Iron"){
+            
+            return parseFloat(current.nutrition_facts.nutrition.vitamines[i].percent) > parseFloat(previous.nutrition_facts.nutrition.vitamines[i].percent) ? current : previous;
+         }
+      }
+   })
+
+   console.log("El donut con mas hierro es " + donutWithMoreIron.name);
 })
 .catch(function (error){
 
    console.log("Hubo un problema con la petición Fetch:" + error.message);
-});
+})
